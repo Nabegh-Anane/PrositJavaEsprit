10f30fc4d8fa0647b8ce129da44787745876330d
Nabegh ANANE <contact@nabeghanane.com>
2024-09-20 16:05:00 +0100

    completer questions 7 et 8

--- src/Produit.java
+++ src/Produit.java
@@ -1,79 +1,98 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Produit {
     int id;
     String lib;
     String brand;
-    double Price;
-
+    double price;
+    Date dateExpiration;
+    
     public Produit() {
         this.id = 0;
         this.lib = "";
         this.brand = "";
-        this.Price = 0.0;
+        this.price = 0.0;
+        this.dateExpiration = new Date(); // date courante par defaut
     }
 
-    public Produit(int id, String lib, String brand, double Price) {
+
+    public Produit(int id, String lib, String brand, double price, Date dateExpiration) {
         this.id = id;
         this.lib = lib;
         this.brand = brand;
-        this.Price = Price;
+        this.price = price;
+        this.dateExpiration = dateExpiration;
     }
 
+
     public void afficher() {
+        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println("Identifiant : " + id);
         System.out.println("Libellé : " + lib);
         System.out.println("Marque : " + brand);
-        System.out.println("Price : " + Price);
+        System.out.println("Prix : " + price);
+        System.out.println("Date d'expiration : " + sdf.format(dateExpiration));
         System.out.println("********************");
     }
 
-    public void modifPrice(double Price) {
-        this.Price = Price;
+
+    public void modifPrice(double price) {
+        this.price = price;
     }
 
-    public boolean iSmanquantInfos() {
-        return this.Price == 0.0;
+
+    public boolean isManquantInfos() {
+        return this.price == 0.0;
     }
+
+
+    @Override
+    public String toString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return "Produit [ID=" + id + ", Libellé=" + lib + ", Marque=" + brand + ", Prix=" + price + ", Date d'expiration=" + sdf.format(dateExpiration) + "]";
+    }
+
     public static void main(String[] args) {
-        Produit a = new Produit(1021, "Lait", "Delice", 0.0);
-        Produit b = new Produit(2510, "Yaourt", "Vitalait", 0.0);
-        Produit c = new Produit(3250, "Tomate", "Sicam", 1.2);
+        // Initializing expiration dates
+        Date date1 = new Date(2024 - 1900, 9, 30);
+        Date date2 = new Date(2024 - 1900, 10, 5);
+        Date date3 = new Date(2025 - 1900, 1, 15);
 
-        // question 4
+        // Creating products
+        Produit a = new Produit(1021, "Lait", "Delice", 0.0, date1);
+        Produit b = new Produit(2510, "Yaourt", "Vitalait", 0.0, date2);
+        Produit c = new Produit(3250, "Tomate", "Sicam", 1.2, date3);
+
+        // question 3
         a.afficher();
         b.afficher();
         c.afficher();
 
-        // question 4
-        a.modifPrice(0.7);
-
-
-        //question 5
-        if (a.iSmanquantInfos()) {
+        // question 4 & 5
+        if (a.isManquantInfos()) {
             a.modifPrice(0.7);
         }
-        if (b.iSmanquantInfos()) {
+        if (b.isManquantInfos()) {
             b.modifPrice(0.5);
         }
 
-        //question 7 Afficher les produits modifiés.
+        // question 6
         System.out.println("Produits modifiés:");
-        if (!a.iSmanquantInfos()) {
+        if (!a.isManquantInfos()) {
             a.afficher();
         }
-        if (!b.iSmanquantInfos()) {
+        if (!b.isManquantInfos()) {
             b.afficher();
         }
-        if (!c.iSmanquantInfos()) {
+        if (!c.isManquantInfos()) {
             c.afficher();
         }
+
+        // question 7
+        System.out.println("Produits créés:");
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+        System.out.println(c.toString());
     }
 }
-
-
-
-
-
-
-
-
-
